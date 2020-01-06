2cb8cd9b19c698df86bca35a6703019624726e4d
ibrahim Yazar <ibrahimyazarusa@gmail.com>
2020-01-05 23:13:16 -0500

    created packages and files

---
+++ src/test/java/com/odoo/pages/BasePage.java
@@ -0,0 +1,15 @@
+package com.odoo.pages;
+
+import com.odoo.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public class BasePage {
+
+    public BasePage() {
+        //this method requires to provide webdriver object for @FindBy
+        //and page class
+        //this means this page class
+        PageFactory.initElements(Driver.get(), this);
+    }
+
+}
