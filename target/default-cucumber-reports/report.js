50aa73e2ed99fd774c84b6c7e9a368d37fe38274
madinaturtbaeva <madina.turtbaeva@gmail.com>
2020-01-13 14:23:04 -0500

    update

--- target/default-cucumber-reports/report.js
+++ target/default-cucumber-reports/report.js
@@ -1,11 +1,16 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/features/crm_module/Quotations.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/features/contacts_module/Contacts.feature");
 formatter.feature({
-  "name": "Quotations Module Functionality",
+  "name": "Contacts page testing",
   "description": "",
-  "keyword": "Feature"
+  "keyword": "Feature",
+  "tags": [
+    {
+      "name": "@contacts_page"
+    }
+  ]
 });
 formatter.background({
-  "name": "user logs in as eventscrmmanager2 and clicks at the \"CRM\" and \"Quotations\" modules",
+  "name": "user is on the login page",
   "description": "",
   "keyword": "Background"
 });
@@ -22,9 +27,22 @@ formatter.match({
 formatter.result({
   "status": "passed"
 });
+formatter.scenario({
+  "name": "Create a new contact",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@contacts_page"
+    },
+    {
+      "name": "@create_a_contact"
+    }
+  ]
+});
 formatter.step({
-  "name": "user logs in as \"eventscrmmanager2\"",
-  "keyword": "When "
+  "name": "user logs in as \"pos5\"",
+  "keyword": "Given "
 });
 formatter.match({
   "location": "CRM_StepDefinitions.user_logs_in_as(String)"
@@ -33,81 +51,233 @@ formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "user navigates to \"CRM\"",
-  "keyword": "And "
+  "name": "user navigates to Contacts module",
+  "keyword": "Then "
 });
 formatter.match({
-  "location": "CRM_StepDefinitions.user_navigates_to(String)"
+  "location": "ContactsStepDefinitions.user_navigates_to_module()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "user navigates to Quotations",
+  "name": "user creates a contact",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "CRM_StepDefinitions.user_navigates_to_Quotations()"
+  "location": "ContactsStepDefinitions.user_creates_a_contact()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.background({
+  "name": "user is on the login page",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user is on the login page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "ContactsStepDefinitions.user_is_on_the_login_page()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.scenario({
-  "name": "Verify that user is able to check one of the quotations, save it as a template and send a recovery email",
+  "name": "Delete a contact",
   "description": "",
   "keyword": "Scenario",
   "tags": [
     {
-      "name": "@Send_A_Card_Recovery_Email"
+      "name": "@contacts_page"
+    },
+    {
+      "name": "@delete_a_contact"
     }
   ]
 });
 formatter.step({
-  "name": "user checks the first present checkbox",
-  "keyword": "And "
+  "name": "user logs in as \"pos1\"",
+  "keyword": "Given "
 });
 formatter.match({
-  "location": "CRM_StepDefinitions.user_checks_the_first_present_checkbox()"
+  "location": "CRM_StepDefinitions.user_logs_in_as(String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "user clicks at Print and Action buttons to see the options",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "CRM_StepDefinitions.user_clicks_at_Print_and_Action_buttons_to_see_the_options()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user clicks at \"send a recovery email\" option",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "CRM_StepDefinitions.user_clicks_at_option(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user should save it as a new template",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "CRM_StepDefinitions.user_should_save_it_as_a_new_template()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user clicks at the send button to send the recovery email",
+  "name": "user navigates to Contacts module",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "CRM_StepDefinitions.user_clicks_at_the_send_button_to_send_the_recovery_email()"
+  "location": "ContactsStepDefinitions.user_navigates_to_module()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "search and delete \"elton john\" contact",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "ContactsStepDefinitions.search_and_delete_contact(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.background({
+  "name": "user is on the login page",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user is on the login page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "ContactsStepDefinitions.user_is_on_the_login_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Verify  that check box is clicked",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@contacts_page"
+    },
+    {
+      "name": "@Verify_checkbox_isClicked"
+    }
+  ]
+});
+formatter.step({
+  "name": "user is on the login page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "ContactsStepDefinitions.user_is_on_the_login_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user logs in as inventory_manager",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "ContactsStepDefinitions.user_logs_in_as_inventory_manager()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user navigates to Contacts module",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "ContactsStepDefinitions.user_navigates_to_module()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "verify that the top checkbox is clicked",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "ContactsStepDefinitions.verify_that_the_top_checkbox_is_clicked()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.background({
+  "name": "user is on the login page",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user is on the login page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "ContactsStepDefinitions.user_is_on_the_login_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Verify that display",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@contacts_page"
+    },
+    {
+      "name": "@Verify_message_isDisplayed"
+    }
+  ]
+});
+formatter.step({
+  "name": "user is on the login page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "ContactsStepDefinitions.user_is_on_the_login_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user logs in as inventory_manager",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "ContactsStepDefinitions.user_logs_in_as_inventory_manager()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user navigates to Contacts module",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "ContactsStepDefinitions.user_navigates_to_module()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "verify that message is displayed",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "ContactsStepDefinitions.verify_that_message_is_displayed()"
 });
 formatter.result({
   "status": "passed"
